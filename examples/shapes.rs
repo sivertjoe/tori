5945b13f1b5abccccf660e7cd98f555381c0cccc
sivertjoe <sivert-joh@hotmail.com>
2023-09-02 21:42:25 +0200

    update: replace example
    use a general shape example instead of a triangle example

--- examples/shapes.rs
+++ examples/shapes.rs
@@ -1,5 +1,5 @@
 use tori::{
-    graphics::shape::Triangle,
+    graphics::shape::{Rect, Triangle},
     math::{Point, Vec4},
     window::Window,
 };
@@ -9,8 +9,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
 
     let triangle = Triangle::new(Point::new(150, 150), Point::new(200, 200), Point::new(250, 150));
 
-    let mut triangle2 = Triangle::new(Point::new(0, 0), Point::new(50, 50), Point::new(0, 50));
-    triangle2.set_color(Vec4::new(0.0, 1.0, 0.0, 1.0));
+    let mut rect = Rect::new(50, 50, 75, 75);
+    rect.set_color(Vec4::new(0., 0.5, 0.5, 1.0));
 
     while window.is_open()
     {
@@ -19,7 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
         window.clear();
 
         window.draw(&triangle);
-        window.draw(&triangle2);
+        window.draw(&rect);
 
         window.swap_buffers();
     }
