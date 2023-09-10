79c08664c749278a06765e73a8c554615362718a
sivertjoe <sivert-joh@hotmail.com>
2023-09-10 16:15:17 +0200

    update: text example

--- examples/text.rs
+++ examples/text.rs
@@ -8,35 +8,59 @@ use tori::{
 
 fn main() -> Result<(), Box<dyn std::error::Error>>
 {
-    let mut window = Window::new("Hello", 300, 300)?;
+    let mut window = Window::new("Hello", 400, 400)?;
 
     let ascii = window.load_font("OpenSans-Regular.ttf", CharSet::Ascii)?;
-    let custom = window
-        .load_font("PretendardVariable.ttf", CharSet::Custom(Box::new(['と', 'り'].into_iter())))?;
 
-    let mut time = Instant::now();
+    let jap = window.load_font(
+        "jap.ttf",
+        CharSet::Custom(Box::new(
+            [
+                '今', '朝', '、', '鳥', 'が', 'き', 'れ', 'い', 'に', '鳴', 'い', 'て', 'い', 'ま',
+                'し', 'た',
+            ]
+            .into_iter(),
+        )),
+    )?;
+
+    let all = window.load_font("OpenSans-Italic.ttf", CharSet::All)?;
+
+    let mut last_time = Instant::now();
     let mut n_frames = 0;
 
-    let mut text = Text::new(&ascii, "starting...", 0.0, 0.0, 0.75, math::vec3(1.0, 1.0, 0.5));
-    let custom_text = Text::new(&custom, "とり", 0.0, 100.0, 1.0, math::vec3(0.4, 0.7, 0.3));
+    let mut fps = Text::new(&ascii, "starting...", 0.0, 250.0, 0.75, math::vec3(1.0, 1.0, 0.5));
+
+    let jap = Text::new(
+        &jap,
+        "今朝、鳥がきれいに鳴いていました",
+        0.0,
+        150.0,
+        0.5,
+        math::vec3(0.0, 0.75, 0.45),
+    );
+
+    let text = Text::new(&all, "æøå", 0.0, 0.0, 1.5, math::vec3(0.3, 0.5, 0.1));
+
 
     while window.is_open()
     {
         n_frames += 1;
-        if time.elapsed().as_millis() >= 1000
+        if last_time.elapsed().as_millis() >= 1000
         {
             let t = 1000.0 / (n_frames as f32);
-            text.text = format!("{:.2} ms/frame", t);
-            time = Instant::now();
+            fps.text = format!("{:.2} ms/frame", t);
+            last_time = Instant::now();
             n_frames = 0;
         }
+
 
         let _ = window.poll_events();
 
         window.clear();
 
+        window.draw(&fps);
+        window.draw(&jap);
         window.draw(&text);
-        window.draw(&custom_text);
 
         window.swap_buffers();
     }
