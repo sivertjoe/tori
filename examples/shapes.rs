6ad98c6232be9f256db77b064fcefb00973c976b
sivertjoe <sivert-joh@hotmail.com>
2023-09-11 18:32:18 +0200

    fix: shape example

--- examples/shapes.rs
+++ examples/shapes.rs
@@ -1,16 +1,16 @@
 use tori::{
     graphics::shape::{Rect, Triangle},
-    math::{Point, Vec4},
+    math::{vec2, vec4},
     window::Window,
 };
 fn main() -> Result<(), Box<dyn std::error::Error>>
 {
     let window = Window::new("Hello", 300, 300)?;
 
-    let triangle = Triangle::new(Point::new(150, 150), Point::new(200, 200), Point::new(250, 150));
+    let triangle = Triangle::new(vec2(150.0, 150.0), vec2(200.0, 200.0), vec2(250.0, 150.0));
 
-    let mut rect = Rect::new(50, 50, 75, 75);
-    rect.set_color(Vec4::new(0., 0.5, 0.5, 1.0));
+    let mut rect = Rect::new(50.0, 50.0, 75.0, 75.0);
+    rect.set_color(vec4(0., 0.5, 0.5, 1.0));
 
     while window.is_open()
     {
